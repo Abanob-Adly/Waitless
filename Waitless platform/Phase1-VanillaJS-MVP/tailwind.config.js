2a9232ee805e297866e465aceb46fc57dee53760
Mohamed Eid <mohamedeid052@gmail.com>
2026-03-19 18:13:59 +0200

    Continue setup: - Added a how to run comment in taliwind.css - add output.css to .gitignore

--- Waitless platform/Phase1-VanillaJS-MVP/tailwind.config.js
+++ Waitless platform/Phase1-VanillaJS-MVP/tailwind.config.js
@@ -1,3 +1,10 @@
+/* ---> To run, excute the following first:
+npm install -D tailwindcss@3
+npx tailwindcss init
+cd 'Waitless platform\Phase1-VanillaJS-MVP'
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+*/
+
 // See: https://v3.tailwindcss.com/docs/installation
 // notice this is tailwindcss v3. 
 // v4 no longer use tailwind.config.js
